a436569a74885facf88011906e80ff20a73f3311
MarcoAtencio <Marco_Atencio@hotmail.com>
2021-05-15 23:53:44 -0500

    first commit

---
+++ database/migrations/2021_05_07_005730_create_pays_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pays', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('proof_payment_id');
+            $table->foreign('proof_payment_id')->references('id')->on('proof_payments');
+            $table->boolean('state');
+            $table->double('debt');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pays');
+    }
+}
